45ebb6aeb86768a46b09c38d691535f069a48095
Clint <maxeva02@gmail.com>
2021-03-01 02:39:57 -0800

    pulling api notes from js file

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const apiRoutes = require("./routes/apiRoutes")
 
 const notes = express();
 const PORT = 9005;
 const mainDirectory = path.join(__dirname, "/public");
 
 notes.use(express.static("public"));
+//from activities folder
 notes.use(express.urlencoded({ extended: true }));
 notes.use(express.json());
 
@@ -18,6 +20,16 @@ notes.get("*", function(reg, res) {
     res.sendFile(path.join(mainDirectory, "index.html"));
 });
 
+//fetching api from JS file
+notes.post("/api/notes", function(reg, res) {
+    let savedNotes = JSON.parse(fs.readFile(".db/db.json"));
+    let newNote = req.body;
+    let UniqueID = (savedNotes.length).toString();
+    newNote.id = UniqueID;
+    savedNotes.push(newNote);
+})
+
+//console logging for the return if no errors to tell user the port is active
 notes.listen(PORT, function() {
-    console.log(`App is listening on ${PORT}`)
+    console.log(`App is listening on PORT ${PORT}`)
 })
