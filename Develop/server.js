f1e8d666bb7cf867fffb0fb231b9edbec804717c
Clint <maxeva02@gmail.com>
2021-03-01 02:16:12 -0800

    tested port after attaching the first html file

--- Develop/server.js
+++ Develop/server.js
@@ -5,3 +5,15 @@ const fs = require("fs");
 const notes = express();
 const PORT = 9005;
 const mainDirectory = path.join(__dirname, "/public");
+
+notes.use(express.static("public"));
+notes.use(express.urlencoded({ extended: true }));
+notes.use(express.json());
+
+notes.get("/notes", function(reg, res) {
+    res.sendFile(path.join(mainDirectory, "notes.html"));
+});
+
+notes.listen(PORT, function() {
+    console.log(`App is listening on ${PORT}`)
+})
